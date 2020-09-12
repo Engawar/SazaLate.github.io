51edc90c13025379d2166537984cf5e4ddfc8bce
SazaLate <32690548+SazaLate@users.noreply.github.com>
2020-09-12 18:02:12 +0900

    Ver.1.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,6 @@
-document.querySelector('p').onclick = function(){
-    alert('裏ページに進みますか？　Yes/No')
-}
-
-var myHTML = document.querySelector('li');
+var myHTML = document.querySelector('h6');
 myHTML.onclick = function(){
-    alert('やけどするよ')
+    alert('404 not found.')
 };
 
 
@@ -14,25 +10,31 @@ var myImage = document.querySelector('img');
 myImage.onclick = function(){
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/IMG_5604.JPG'){
-        myImage.setAttribute('src','images/WSML2872.JPG');
+        myImage.setAttribute('src','images/1.JPG');
+    } else if(mySrc === 'images/1.JPG'){
+        myImage.setAttribute('src','images/2.JPG');
+    } else if(mySrc === 'images/2.JPG'){
+        myImage.setAttribute('src','images/3.JPG');
+    } else if(mySrc === 'images/3.JPG'){
+        myImage.setAttribute('src','images/4.JPG');
     } else {
         myImage.setAttribute('src','images/IMG_5604.JPG');
-    } 
+    }
 }
 
 
 //　ここからボタンを作って自己紹介を改ざんするプログラム
 var myButton = document.querySelector('button');
-var myHeading = document.querySelector('p');
+var myHeading = document.querySelector('h5');
 
 // ここで関数を作る。setUserName()
 function setUserName(){
-    let myName = prompt('Please enter your introduction.');
+    let myName = prompt('Please enter your message!');
     if(!myName || myName === null){
         setUserName();
     } else {
         localStorage.setItem('name',myName);
-        myHeading.textContent = myName + ' Writer/Programmer';
+        myHeading.textContent = myName + ' /by jon doe.';
     }
 }
 
@@ -40,8 +42,9 @@ if(!localStorage.getItem('name')){
     setUserName();
 } else {
     var storedName = localStorage.getItem('name');
-    myHeading.textContent = storedName + '　Writer/Programmer';
+    myHeading.textContent = storedName + ' /by jon doe.';
 }
 
 myButton.onclick = function(){
     setUserName();
+}
